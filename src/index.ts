82f0a7b97bb4267ecba45631c3b6c0771e83e858
fadezhanger <dzhan96@uwo.ca>
2020-10-14 20:30:00 -0400

    Explicit function return types

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ const replaceMap: Record<string, string> = {'+': '-', '/': '_', '=': ''};
 /** Use 24 bytes of random data so the encoded representation is 32 characters. */
 const randomDataLength = 24;
 
-export function urlSafeBase64(rawBase64: string) {
+export function urlSafeBase64(rawBase64: string): string {
   return rawBase64.replace(urlSafeRegex, (match) => replaceMap[match]);
 }
 
 /**
  * Create a 32-byte CSRF token.
  */
-export function createToken() {
+export function createToken(): string {
   const randomBytes = crypto.randomBytes(randomDataLength);
   return urlSafeBase64(randomBytes.toString('base64'));
 }
@@ -24,7 +24,7 @@ export function createToken() {
  * @param token CSRF token to have checksum generated against.
  * @param secret Shared secret key.
  */
-export function createChecksum(token: string, secret: string) {
+export function createChecksum(token: string, secret: string): string {
   const hmac = crypto.createHmac('sha256', secret).update(token).digest();
   const checksum = urlSafeBase64(hmac.toString('base64'));
   return checksum;
@@ -36,7 +36,7 @@ export function createChecksum(token: string, secret: string) {
  * @param checksum Generated checksum of the original token.
  * @param secret Shared secret key.
  */
-export function verifyChecksum(token: string, checksum: string, secret: string) {
+export function verifyChecksum(token: string, checksum: string, secret: string): boolean {
   if (!token || !checksum || typeof token !== 'string' || typeof checksum !== 'string') {
     return false;
   }
